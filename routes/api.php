71bf79799a3c7ae58bf583da34db4007b3d313a2
olahdavidandras <olahtdavidandras@uni.sapientia.ro>
2025-03-12 17:32:08 +0200

    api.php

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,24 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ImageEnhanceController;
 use App\Http\Controllers\PostController;
-// use App\Http\Controllers\ReplicateController;
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
-
-
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-
-
-Route::post('/enhance', [ImageEnhanceController::class, 'enhance']);
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
+Route::middleware('auth:sanctum')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+    // Route::get('/user', function (Request $request) {
+    //     return $request->user();
+    // });
+    Route::post('/enhance', [ImageEnhanceController::class, 'enhance']);
+    Route::post('/check-status', [ImageEnhanceController::class, 'checkStatus']);
 
+    Route::get('/post', [PostController::class, 'index'])->name('post.index');
+    Route::post('/post/store', [PostController::class, 'store'])->name('post.store');
+    Route::delete('/post/{post}/delete-image', [PostController::class, 'deleteImage'])->name('post.deleteImage');
 
-// Route::post('/enhance', [ImageEnhanceController::class, 'enhance'])->middleware('auth:sanctum');
+    Route::post('/upload-image', [ImageEnhanceController::class, 'uploadImage'])->name('image.upload');
 
+});
